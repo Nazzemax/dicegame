62578630c773c46d43356f426ebce0f96da78a77
Nazzemax <hvan1998@inbox.ru>
2023-05-15 16:13:56 +0600

    feat: added ability to play the game with button

--- script.js
+++ script.js
@@ -3,18 +3,30 @@ const playerImageOne = document.querySelector('.img1');
 const playerImageTwo = document.querySelector('.img2');
 const playerOne = document.querySelectorAll('p')[0].textContent;
 const playerTwo = document.querySelectorAll('p')[1].textContent;
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+const rollButton = document.querySelector('.dice-btn');
 
-playerImageOne.setAttribute('src',`./images/dice${randomNumber1}.png`);
-playerImageTwo.setAttribute('src',`./images/dice${randomNumber2}.png`);
-
-if(randomNumber1 > randomNumber2) {
-    gameTextStatus.textContent = 
-    `${playerOne} has won the game!`;
-} else if (randomNumber2 > randomNumber1) {
-    gameTextStatus.textContent = 
-    `${playerTwo} has won the game!`;
-} else {
-    gameTextStatus.textContent = 'Draw!';
+function startGame () {
+    let randomNumber1 = Math.floor( Math.random() * 6 ) + 1;
+    let randomNumber2 = Math.floor( Math.random() * 6 ) + 1;
+    playerImageOne.setAttribute( 'src', `./images/dice${randomNumber1}.png` );
+    playerImageTwo.setAttribute( 'src', `./images/dice${randomNumber2}.png` );
+    checkGameCondition( randomNumber1, randomNumber2 );
 }
+
+function checkGameCondition (randomNumber1,randomNumber2) {
+    if(randomNumber1 > randomNumber2) {
+        gameTextStatus.textContent = 
+        `${playerOne} has won the game!`;
+    } else if (randomNumber2 > randomNumber1) {
+        gameTextStatus.textContent = 
+        `${playerTwo} has won the game!`;
+    } else {
+        gameTextStatus.textContent = 'Draw!';
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    rollButton.addEventListener('click', () => {
+        startGame();
+    })
+})
