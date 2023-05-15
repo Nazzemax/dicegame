93ff461ab7d9a45b58e3b8ecd5c68ecdc177fe57
Nazzemax <hvan1998@inbox.ru>
2023-05-15 15:22:07 +0600

    feat: players now have random images

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const gameTextStatus = document.querySelector('h1');
 const playerImageOne = document.querySelector('.img1');
 const playerImageTwo = document.querySelector('.img2');
-let randomNumber1 = Math.floor(Math.random() * 6);
-let randomNumber2 = Math.floor(Math.random() * 6);
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+playerImageOne.setAttribute('src',`./images/dice${randomNumber1}.png`);
+playerImageTwo.setAttribute('src',`./images/dice${randomNumber2}.png`);
