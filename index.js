4f78f6e8f6a794b26e440bdfc6ac040884a1b496
Nazzemax <hvan1998@inbox.ru>
2023-05-15 15:17:24 +0600

    feat: added two variables  that make random number from 1 to 6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const gameTextStatus = document.querySelector('h1');
 const playerImageOne = document.querySelector('.img1');
 const playerImageTwo = document.querySelector('.img2');
-
+let randomNumber1 = Math.floor(Math.random() * 6);
+let randomNumber2 = Math.floor(Math.random() * 6);
